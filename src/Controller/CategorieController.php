a205e61833a2a9148ba9b4f772af19d168c13b6f
Vapie <valentin.tapie@etu.univ-smb.fr>
2021-04-24 12:42:40 +0200

    added filtered vue insttr/categorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Entity\Riff;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,4 +20,23 @@ class CategorieController extends AbstractController
             'controller_name' => 'CategorieController',
         ]);
     }
+
+    /**
+     * @Route("/categorie/{id}", name="categorie_show")
+     */
+    public function show(EntityManagerInterface $entityManager, $id): Response
+    {
+        $categorie = $entityManager->getRepository(Categorie::class)->findOneBy(['id'=> $id]);
+//        if ( $categorie != null ){
+//            Categorie::$categorie
+//        }
+
+        $riffs = $entityManager->getRepository(Riff::class)->findBy( ['categorie'=> $id ]);
+        return $this->render('categorie/show.html.twig', ['controller_name' => 'CategorieController',"riffs"=>$riffs,"categorie"=>$categorie]);
+    }
+
+
+
+
+
 }
--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Entity\Instrument;
+use App\Entity\Riff;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +20,16 @@ class InstrumentController extends AbstractController
         return $this->render('instrument/index.html.twig', [
             'controller_name' => 'InstrumentController',
         ]);
+    }
+
+    /**
+     * @Route("/instrument/{id}", name="instrument_show")
+     */
+    public function show(EntityManagerInterface $entityManager, $id): Response
+    {
+        $instrument = $entityManager->getRepository(Instrument::class)->findOneBy(['id'=> $id]);
+        $riffs = $entityManager->getRepository(Riff::class)->findBy( ['instrument'=> $id ]);
+        return $this->render('instrument/show.html.twig', ['controller_name' => 'InstrumentController',"riffs"=>$riffs,"instrument"=>$instrument]);
+
     }
 }
